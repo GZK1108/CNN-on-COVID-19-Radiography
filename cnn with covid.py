93bedc8e77139cdea2aa3506df096be3f9af0d47
GZK1108 <stanleywell1229@gmail.com>
2022-08-01 17:39:07 +0800

    改为split后的数据

--- cnn with covid.py
+++ cnn with covid.py
@@ -9,42 +9,42 @@ import os
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 
-train_root = "C:/Users/11453/PycharmProjects/riskassessment/data/COVID-19_Radiography_Dataset"
-test_root = "C:/Users/11453/PycharmProjects/riskassessment/data/COVID-19_Radiography_Dataset"
+train_root = "C:/Users/11453/PycharmProjects/riskassessment/data/COVID-19 (train test split)/train"
+test_root = "C:/Users/11453/PycharmProjects/riskassessment/data/COVID-19 (train test split)/test"
 
 # batch_size = 1, accuracy is still not the same
 batch_size = 32
 
 Generator = ImageDataGenerator()
-train_data = Generator.flow_from_directory(train_root, (150, 150), batch_size=batch_size)
-test_data = Generator.flow_from_directory(test_root, (150, 150), batch_size=batch_size)
+train_data = Generator.flow_from_directory(train_root, (70, 70), batch_size=batch_size)
+test_data = Generator.flow_from_directory(test_root, (70, 70), batch_size=batch_size)
 
 num_classes = len([i for i in os.listdir(train_root)])
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3), activation='relu'))
+model.add(Conv2D(32, (3, 3), input_shape=(70, 70, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-model.add(Dropout(0.05))
+model.add(Dropout(0.2))
 
 model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-model.add(Dropout(0.05))
+model.add(Dropout(0.2))
 
 model.add(Conv2D(64, (3, 3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-model.add(Dropout(0.05))
+model.add(Dropout(0.2))
 
 model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-model.add(Dropout(0.05))
+model.add(Dropout(0.2))
 
 model.add(Flatten())
 
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.05))
+model.add(Dropout(0.2))
 
 model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.05))
+model.add(Dropout(0.2))
 
 model.add(Dense(num_classes, activation="softmax"))
 model.summary()
@@ -56,11 +56,11 @@ model.fit(train_data, batch_size=batch_size, epochs=12)
 # print(score)
 score = model.evaluate(test_data)
 print(score)
-
+"""
 pred = model.predict(test_data)
 pred_classes = np.argmax(pred, axis=1)
 cm = confusion_matrix(test_data.classes, pred_classes)
 # print(cm)
 print((cm[0,0]+cm[1,1]+cm[2,2])/(sum(sum(cm))))
 sns.heatmap(cm,annot=True)
-plt.show()
+plt.show()"""
