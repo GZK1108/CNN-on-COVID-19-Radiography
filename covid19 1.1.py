26a3d6a970a24f245fe3bfd7bf654ac9c9ff98c4
GZK1108 <stanleywell1229@gmail.com>
2022-08-18 22:37:13 +0800

    完善

--- covid19 1.1.py
+++ covid19 1.1.py
@@ -47,10 +47,10 @@ for id, level in enumerate(levels):
     for file in os.listdir(os.path.join(data_dir, level)):
         data.append(['{}/{}'.format(level, file), level])
 
-data = pd.DataFrame(data, columns=['image_file', 'corona_result'])
+data = pd.DataFrame(data, columns=['image_file', 'result'])
 
 data['path'] = path + '/' + data['image_file']
-data['corona_result'] = data['corona_result'].map({'Normal': 'Negative', 'COVID': 'Positive'})
+data['result'] = data['result'].map({'Normal': 'Negative', 'COVID': 'Positive'})
 samples = 13808
 
 data.head()
@@ -59,12 +59,12 @@ print('Number of Duplicated Samples: %d' % (data.duplicated().sum()))
 print('Number of Total Samples: %d' % (data.isnull().value_counts()))
 
 df = pd.DataFrame()
-df['corona_result'] = ['Positive', 'Negative']
-df['Count'] = [len(data[data['corona_result'] == 'Positive']), len(data[data['corona_result'] == 'Negative'])]
+df['result'] = ['Positive', 'Negative']
+df['Count'] = [len(data[data['result'] == 'Positive']), len(data[data['result'] == 'Negative'])]
 df = df.sort_values(by=['Count'], ascending=False)
 
-fig = px.bar(df, x='corona_result', y='Count',
-             color="corona_result", text_auto='', width=600,
+fig = px.bar(df, x='result', y='Count',
+             color="result", text_auto='', width=600,
              color_discrete_sequence=["orange", "purple"],
              template='plotly_dark')
 
@@ -82,7 +82,7 @@ n_samples = 3
 
 fig, m_axs = plt.subplots(2, n_samples, figsize=(6 * n_samples, 3 * 4))
 
-for n_axs, (type_name, type_rows) in zip(m_axs, data.sort_values(['corona_result']).groupby('corona_result')):
+for n_axs, (type_name, type_rows) in zip(m_axs, data.sort_values(['result']).groupby('result')):
     n_axs[1].set_title(type_name, fontsize=15)
     for c_ax, (_, c_row) in zip(n_axs, type_rows.sample(n_samples, random_state=1234).iterrows()):
         picture = c_row['path']
--- covid19 1.4.py
+++ covid19 1.4.py
@@ -79,9 +79,9 @@ x = np.array(x)
 y = np.array(y)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1, random_state=42)
+# x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1, random_state=42)
 
-print(x_train.shape, x_test.shape, x_val.shape, y_train.shape, y_test.shape, y_val.shape)
+# print(x_train.shape, x_test.shape, x_val.shape, y_train.shape, y_test.shape, y_val.shape)
 
 
 model = load_model("C:/Users/11453/PycharmProjects/riskassessment/resnetCOVID19.h5")
@@ -92,17 +92,17 @@ pred = np.argmax(pred, axis=1)
 cm = confusion_matrix(y_test, pred)
 
 
-plt.figure(figsize=(12, 9), dpi=60)
-x_ticks =['Negitive','Positive']
-y_ticks =['Negitive','Positive']
-ax = sns.heatmap(data=cm, xticklabels=x_ticks,yticklabels=y_ticks,annot=True, fmt='d', annot_kws={"fontsize":20}, cmap='Blues')
+plt.figure(figsize=(12, 9), dpi=80)
+x_ticks =['Negative','Positive']
+y_ticks =['Negative','Positive']
+ax = sns.heatmap(data=cm, xticklabels=x_ticks, yticklabels=y_ticks,annot=True, fmt='d', annot_kws={"fontsize":20}, cmap='Blues')
 ax.set_title('Confusion matrix',fontsize=20)  # 图标题
 ax.set_xlabel('Predict',fontsize=15)  # x轴标题
 ax.set_ylabel('True',fontsize=15)
-# plt.xticks(fontsize=50)
-# plt.yticks(fontsize=50)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
 
-# plt.savefig('confusion matrix.png')
+plt.savefig('confusion matrix.png')
 
 print("accuracy:",round(accuracy_score(y_test, pred, normalize=True, sample_weight=None),3))
 print("precision:",round(precision_score(y_test, pred, average='binary'),3))  # 测试集精确率
